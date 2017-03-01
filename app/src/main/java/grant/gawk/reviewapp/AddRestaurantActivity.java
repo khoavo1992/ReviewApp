658a969ac0951bea4134abe8dafb49eee68057c3
khoavo1992 <kevin.vo1992@gmail.com>
2017-03-01 13:22:07 -0600

    Update AddRestaurantActivity.java

--- app/src/main/java/grant/gawk/reviewapp/AddRestaurantActivity.java
+++ app/src/main/java/grant/gawk/reviewapp/AddRestaurantActivity.java
@@ -2,6 +2,7 @@ package grant.gawk.reviewapp;
 
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -17,9 +18,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class AddRestaurantActivity extends AppCompatActivity implements OnClickListener {
+public class addRestaurant extends AppCompatActivity implements OnClickListener {
     private static final int RESULT_LOAD_IMAGE = 1;
-    private ListView lvRestaurant;
     ImageView getImage;
     Button btnSubmit;
     EditText restName, cityName;
@@ -30,9 +30,17 @@ public class AddRestaurantActivity extends AppCompatActivity implements OnClickL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_restaurant);
 
-        lvRestaurant = (ListView)findViewById(R.id.listRestaurant);
+        getImage = (ImageView) findViewById(R.id.getImage);
+        getImage.setOnClickListener(this);
 
-        List<Restaurant> listRest = new ArrayList<Restaurant>();
+        restName = (EditText) findViewById(R.id.restName);
+        cityName = (EditText) findViewById(R.id.cityName);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+
+        List<restaurantData> listRest = new ArrayList<restaurantData>();
+        restaurantAdapter adapter = new restaurantAdapter(this, listRest);
+
     }
 
 
@@ -42,7 +50,7 @@ public class AddRestaurantActivity extends AppCompatActivity implements OnClickL
         switch (v.getId()) {
             case R.id.getImage:
                 Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI); //Allow the gallery to be opened
-                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE); //pass result_load_image to startActivity (allow user to get to image user has selected)
                 break;
             case R.id.btnSubmit:
                 break;
@@ -51,13 +59,18 @@ public class AddRestaurantActivity extends AppCompatActivity implements OnClickL
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) //make sure to receive result from gallery
+
+        switch(requestCode)
         {
-            Uri selectedImage = data.getData(); //Get the uri, allows to prefer back to the image
-            getImage.setImageURI(selectedImage); //display the image
+        case RESULT_LOAD_IMAGE:
+            if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) //make sure to receive result from gallery (RESULT_LOAD_IMAGE)
+            {
+                Uri selectedImage = data.getData(); //Get the uri, allows to prefer back to the image
+                getImage.setImageURI(selectedImage); //display the image
+            }
+            break;
         }
     }
 
